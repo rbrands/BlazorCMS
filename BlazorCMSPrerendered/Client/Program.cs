14dc8e6bc4f2bbd3481e9a5924296b6aa488a187
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2022-01-02 14:52:11 +0700

    Prerendering for public web site enabled

--- BlazorCMSPrerendered/Client/Program.cs
+++ BlazorCMSPrerendered/Client/Program.cs
@@ -5,8 +5,6 @@ using BlazorCMS.ClientData;
 using BlazorCMS.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
--- BlazorCMSPrerendered/Server/Program.cs
+++ BlazorCMSPrerendered/Server/Program.cs
@@ -35,6 +35,6 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapFallbackToPage("/_Host");
 
 app.Run();
